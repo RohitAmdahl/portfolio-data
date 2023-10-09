2058c711a3223cff0a41810e6813da4034a79750
rohitAmdahl <rohit_designer@outlook.com>
2023-10-10 01:35:21 +0200

    added motion

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -2,6 +2,7 @@ import MyAvatar from "../../assets/avatar_profile_.png";
 import { BsLinkedin, BsGithub, BsEnvelopeAt } from "react-icons/bs";
 import { AiOutlineFilePdf } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const HomePage: React.FC = () => {
   return (
@@ -22,38 +23,50 @@ const HomePage: React.FC = () => {
       </div>
       <div className="container mx-auto max-w-2xl p-4 m-3 border-b-2 border-gray-400">
         <ul className="flex justify-between cursor-pointer m-4">
-          <li
+          <motion.li
             className="px-2 py-2 bg-gray-100 rounded-full hover:text-blue-700 font-bold shadow-xl"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1.2 }}
+            transition={{ duration: 0.5, stiffness: 500 }}
             title="Linkedin"
           >
             <Link to="https://www.linkedin.com/feed/" target="_blank">
               <BsLinkedin size={24} />
             </Link>
-          </li>
-          <li
+          </motion.li>
+          <motion.li
             className="px-2 py-2 bg-gray-100 rounded-full hover:text-blue-700 font-bold shadow-xl"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1.2 }}
+            transition={{ duration: 0.5, stiffness: 500 }}
             title="Github"
           >
             <Link to="https://github.com/RohitAmdahl" target="_blank">
               <BsGithub size={25} />
             </Link>
-          </li>
-          <li
+          </motion.li>
+          <motion.li
             className="px-2 py-2 bg-gray-100 rounded-full hover:text-blue-700 font-bold shadow-xl"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1.2 }}
+            transition={{ duration: 0.5, stiffness: 500 }}
             title="Email"
           >
             <Link to="mailto:rohit_dsigner@outlook.com" target="_blank">
               <BsEnvelopeAt size={25} />
             </Link>
-          </li>
-          <li
+          </motion.li>
+          <motion.li
             className="px-2 py-2 bg-gray-100 shadow-xl rounded-full hover:text-blue-700 font-bold "
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1.2 }}
+            transition={{ duration: 0.5, stiffness: 500 }}
             title="CV Pdf file"
           >
             <span className="">
               <AiOutlineFilePdf size={25} />
             </span>
-          </li>
+          </motion.li>
         </ul>
       </div>
     </>
