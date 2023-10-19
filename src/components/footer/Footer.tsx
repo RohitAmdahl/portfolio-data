65a393f90cb29aaf50e74544f667802f83159484
rohitAmdahl <rohit_designer@outlook.com>
2023-10-20 00:38:20 +0200

    update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer: React.FC = () => {
   return (
-    <div className=" container mx-auto text-center max-w-4xl py-8 m-5 ">
-      <p className="m-8">
+    <div className=" container mx-auto text-center max-w-4xl py-8 m-5 text-link ">
+      <a className="m-8" href="https://www.linkedin.com/feed/">
         Portfolio website created By
         <span className="font-bold"> Rohit kumar Amdahl</span>
-      </p>
+      </a>
     </div>
   );
 };
