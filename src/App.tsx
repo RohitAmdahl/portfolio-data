e0eb5b8b5ad76e146ba11e97d3c805e0134543f8
rohitAmdahl <rohit_designer@outlook.com>
2023-10-24 14:58:04 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 const HomePage = lazy(() => import("./pages/Home"));
 
-const Portfolio = lazy(() => import("./pages/Portfolio"));
+const About = lazy(() => import("./pages/Portfolio"));
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
             <Route
               path="*"
               element={
