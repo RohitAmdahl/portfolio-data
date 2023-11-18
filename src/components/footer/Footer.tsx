32bd70ab6703552af1f87001adf8da6d14367060
rohitAmdahl <rohit_designer@outlook.com>
2023-11-18 02:05:30 +0100

    code update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,10 +3,8 @@ const Footer: React.FC = () => {
     <div className=" container mx-auto text-center max-w-4xl py-8 m-5 text-link ">
       <a className="m-8" href="https://www.linkedin.com/feed/">
         Portfolio website created By
-        <span className="font-bold"> &copy; Rohit kumar Amdahl</span>
-        <span className="font-bold">
-          React, TypeScript, Tailwind CSS, Vite.
-        </span>
+        <span className="font-bold"> &copy; Rohit kumar Amdahl, </span>
+        <p className="font-bold">React, TypeScript, Tailwind CSS, Vite.</p>
       </a>
     </div>
   );
