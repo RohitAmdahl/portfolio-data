03358118c11a54bf0f37935f7cf2206208341db9
rohitAmdahl <rohit_designer@outlook.com>
2023-12-07 00:27:31 +0100

    update name

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -20,7 +20,7 @@ const HomePage: React.FC = () => {
           </div>
           <div>
             <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl tracking-wide pb-2 font-serif">
-              Rohit Kumar Amdahl!
+              Rohit Kumar Amdahl
             </h1>
             <h2 className="font-bold text-2xl tracking-wide pb-2 font-serif">
               Frontend Developer
