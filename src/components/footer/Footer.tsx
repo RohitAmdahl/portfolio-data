84a03c02e1a7594ac0f3b7e9990d15ca5f5e1cce
rohitAmdahl <rohit_designer@outlook.com>
2024-01-28 17:00:01 +0100

    footer color change

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer: React.FC = () => {
   return (
-    <div className=" container mx-auto text-center max-w-4xl py-8 m-5  ">
-      <a className="m-8" href="https://www.linkedin.com/feed/">
-        Portfolio website created By
-        <span className="font-bold"> &copy; Rohit kumar Amdahl, </span>
-        <p className="font-bold">React, TypeScript, Tailwind CSS, Vite.</p>
-      </a>
+    <div className="bg-gray-800 text-white">
+      <div className=" container mx-auto text-center max-w-4xl py-8   ">
+        <a className="m-8" href="https://www.linkedin.com/feed/">
+          Portfolio website created By
+          <span className="font-bold"> &copy; Rohit kumar Amdahl, </span>
+          <p className="font-bold">React, TypeScript, Tailwind CSS, Vite.</p>
+        </a>
+      </div>
     </div>
   );
 };
