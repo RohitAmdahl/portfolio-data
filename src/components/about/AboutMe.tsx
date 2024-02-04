86c5b9d50e44a95b5cfa02c2b101ec1a7b326b0a
rohitAmdahl <rohit_designer@outlook.com>
2024-02-05 00:12:39 +0100

    to do for planing about section

--- src/components/about/AboutMe.tsx
+++ src/components/about/AboutMe.tsx
@@ -4,6 +4,15 @@ const AboutMe = () => {
       <p className="text-3xl font-sixtyfour pt-4 ">
         Page is under construction 🚧
       </p>
+      <p>
+        <ul>
+          <li>banner picture and write about my work</li>
+          <li>java code picture and text about java</li>
+          <li>books i ma reading and learning new things</li>
+          <li>always learning and sharing knowledge is my passion</li>
+          <li>my new website persnal porfolio</li>
+        </ul>
+      </p>
     </div>
   );
 };
