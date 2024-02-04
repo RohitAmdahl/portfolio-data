cb6039682a3859f3b3c25351affa8a9c4c74d32d
rohitAmdahl <rohit_designer@outlook.com>
2024-02-04 23:45:36 +0100

    removed the extra old divs

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,4 +1,3 @@
-
 import myPic from "../../assets/rohitImg.jpg";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
@@ -7,7 +6,6 @@ import { Link } from "react-router-dom";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaDiscord } from "react-icons/fa";
 import Publication from "./Publication";
-
 
 const HomePage: React.FC = () => {
   return (
@@ -110,27 +108,6 @@ const HomePage: React.FC = () => {
           </div>
         </div>
       </section>
-      <div className="m-10 text-text_ ">
-        <div className="  max-w-2xl container mx-auto p-10 font-bold">
-          <p className="text-2xl text-center">
-            Exciting Changes Underway! This site is currently undergoing
-            renovations to bring you an even better experience. Stay tuned!
-          </p>
-        </div>
-      </div>
-      <div className="text-white container mx-auto max-w-2xl  flex flex-col justify-center items-center ">
-        <div className="border-b-2 border-blue-500 pb-4 text-lg">
-          <p className="py-1">
-            <Link
-              className=" px-2 text-link font-semibold hover:underline hover:italic"
-              to="https://github.com/RohitAmdahl"
-              target="_blank"
-            >
-              GitHub Link. 🎨💻,. 🚀
-            </Link>
-          </p>
-        </div>
-      </div>
     </>
   );
 };
