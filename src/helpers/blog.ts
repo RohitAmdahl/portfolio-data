468b69b9c0dca5594013f28e2c7f730c02513aa8
rohitAmdahl <rohit_designer@outlook.com>
2024-02-04 16:12:31 +0100

    working on the interface

---
+++ src/helpers/blog.ts
@@ -0,0 +1,19 @@
+export interface Blog {
+  id: number;
+  title: string;
+  description: string;
+  imgUrl: string;
+  date: string;
+  link: string;
+}
+
+export const blogs: Blog[] = [
+  {
+    id: 1,
+    title: "How to use the Trello API in React Native",
+    description: "",
+    imgUrl: "",
+    date: "",
+    link: "",
+  },
+];
