7e1e9da12df4c0629848a38ebf6e1e2757e1fffd
rohitAmdahl <rohit_designer@outlook.com>
2024-02-04 17:32:23 +0100

    edit footer styles and placement

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Footer: React.FC = () => {
   return (
     <div className="bg-gray-800 text-white">
       <div className=" container mx-auto text-center max-w-4xl py-8   ">
-        <a className="m-8" href="https://www.linkedin.com/feed/">
+        <Link
+          to={"https://www.linkedin.com/feed/"}
+          className="hover:underline m-8"
+        >
           Portfolio website created By
           <span className="font-bold"> &copy; Rohit kumar Amdahl, </span>
-          <p className="font-bold">React, TypeScript, Tailwind CSS, Vite.</p>
-        </a>
+        </Link>
+        <p className="font-bold">React, TypeScript, Tailwind CSS, Vite.</p>
+        <span>{new Date().getFullYear()} </span>
       </div>
     </div>
   );
