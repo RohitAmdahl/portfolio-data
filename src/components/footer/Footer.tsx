a2bd1ab9309a3f59c3e346988a978f559d62419b
rohitAmdahl <rohit_designer@outlook.com>
2024-02-05 19:25:56 +0100

    gav the right tag header and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
-    <div className="bg-gray-800 text-white">
+    <footer className="bg-gray-800 text-white">
       <div className=" container mx-auto text-center max-w-4xl py-8   ">
         <Link
           to={"https://www.linkedin.com/feed/"}
@@ -14,7 +14,7 @@ const Footer: React.FC = () => {
         <p className="font-bold">React, TypeScript, Tailwind CSS, Vite.</p>
         <span>{new Date().getFullYear()} </span>
       </div>
-    </div>
+    </footer>
   );
 };
 
