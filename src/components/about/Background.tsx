60bc9c83e5cad73d082b4666a1d45e7a338beda0
rohitAmdahl <rohit_designer@outlook.com>
2024-02-07 10:52:59 +0100

    edit the text for my background

--- src/components/about/Background.tsx
+++ src/components/about/Background.tsx
@@ -4,7 +4,9 @@ const Background = () => {
   return (
     <>
       <div className="p-4">
-        <h3 className="font-roboto text-xl py-4">My background</h3>
+        <h3 className="font-roboto text-xl py-4">
+          My background with Innovative Techniques
+        </h3>
         <p>
           I have a strong background in architecture, with valuable experience
           gained from working in architecture firms. My specialization lies in
@@ -12,7 +14,12 @@ const Background = () => {
           Modeling) software such as Autodesk Revit, AutoCAD, ArchiCAD, and
           Twinmotion. Additionally, I am proficient in using Architerra for
           generating 3D terrain, utilizing x and y coordinates based on the
-          Norwegian maps coordinate system.
+          Norwegian maps coordinate system. Furthermore, I have leveraged
+          cutting-edge technology, including drones like DJI Go, to fly over
+          buildings and capture aerial data. With tools like DroneDeploy, I've
+          transformed this data into detailed 3D models and conducted precise
+          measurements, showcasing my passion for integrating technology into
+          architectural practice.
         </p>
 
         <div className="py-4 flex justify-around flex-wrap items-center">
