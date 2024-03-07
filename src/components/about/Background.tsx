53622eab1b20d2dffeab3dc4e7c04323db83ce08
rohitAmdahl <rohit_designer@outlook.com>
2024-03-07 12:43:38 +0100

    added classes and p tages for more white spaces

--- src/components/about/Background.tsx
+++ src/components/about/Background.tsx
@@ -8,19 +8,21 @@ const Background = () => {
         <h2 className=" font-bold text-xl py-4">
           My background with Innovative Techniques
         </h2>
-        <p>
+        <p className="py-2">
           I have a strong background in architecture, with valuable experience
           gained from working in architecture firms. My specialization lies in
           creating plan drawings and 3D models using BIM (Building Information
           Modeling) software such as Autodesk Revit, AutoCAD, ArchiCAD, and
-          Twinmotion. Additionally, I am proficient in using Architerra for
-          generating 3D terrain, utilizing x and y coordinates based on the
-          Norwegian maps coordinate system. Furthermore, I have leveraged
-          cutting-edge technology, including drones like DJI Go, to fly over
-          buildings and capture aerial data. With tools like DroneDeploy, I've
-          transformed this data into detailed 3D models and conducted precise
-          measurements, showcasing my passion for integrating technology into
-          architectural practice.
+          Twinmotion.
+        </p>
+        <p>
+          I am proficient in using Architerra for generating 3D terrain,
+          utilizing x and y coordinates based on the Norwegian maps coordinate
+          system. Furthermore, I have leveraged cutting-edge technology,
+          including drones like DJI Go, to fly over buildings and capture aerial
+          data. With tools like DroneDeploy, I've transformed this data into
+          detailed 3D models and conducted precise measurements, showcasing my
+          passion for integrating technology into architectural practice.
         </p>
 
         <div className="py-4 flex justify-around flex-wrap items-center">
--- src/components/about/JavaSection.tsx
+++ src/components/about/JavaSection.tsx
@@ -13,23 +13,27 @@ const JavaSection = () => {
             />
             <figcaption>
               <p className="text-md text-center font-light ">
-                Java coding in IntelliJ, practising object-oriented programming.
+                Java coding in IntelliJ, practising object-oriented
+                programming."In short:", "My basis".
               </p>
             </figcaption>
           </figure>
 
           <h2 className="font-bold text-xl py-4">
-            You might have questions about WHY JAVA ?
+            "So why Java?" you might ask. Here's my answer:
           </h2>
-          <p>
+          <p className="py-2">
             I usually attend different meetups because I like to keep myself
             updated. I went to a JavaBin meetup where I found Java quite
             interesting. Although I couldn't fully understand the code during
             the presentation, I could relate it to JavaScript classes and made
             an effort to grasp the concepts. I spoke to many experienced Java
             developers and realized the importance of naming conventions in
-            Java. This led me to decide that I want to learn Java, appreciating
-            its ease of understanding and robust nature, with continuous updates
+            Java.
+          </p>
+          <p>
+            This led me to decide that I want to learn Java, appreciating its
+            ease of understanding and robust nature, with continuous updates
             over the years. Personally, I became a fan of object-oriented
             programming through Java, as it provides a structured and efficient
             way to organize code. This not only makes Java a practical choice
