1ce4251f25e295d9ff094e920d702ce1ba6358a8
rohitAmdahl <rohit_designer@outlook.com>
2024-06-11 21:13:15 +0200

    simple footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
-
 const Footer: React.FC = () => {
   return (
     <footer className="pt-10">
       <div className=" text-center">
         <span className="font-bold"> &copy; Rohit kumar Amdahl, </span>
-
         <span>{new Date().getFullYear()} </span>
       </div>
     </footer>
